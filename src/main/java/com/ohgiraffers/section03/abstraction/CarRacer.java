839c035e04012c4c191fae313766a537ec6c174b
choseungyoun <seungyoun-cho@joseung-yun-ui-MacBookAir.local>
2024-02-19 15:23:43 +0900

    [24년2월19일공부내용]

---
+++ src/main/java/com/ohgiraffers/section03/abstraction/CarRacer.java
@@ -0,0 +1,24 @@
+package com.ohgiraffers.section03.abstraction;
+
+public class CarRacer {
+
+    /*필기. 최초 할당 이후 변경 불가능한 불변의 상태를 나타낸다.
+    *  이를 통해 객체의 안전성과 안전을 보장할 수 있다.
+    *  예기치 않은 상태 변경을 방지하는데 도움이 된다.*/
+    private final Car car = new Car();
+
+    public void startUp() {
+        car.startUp();
+    }
+    public void stepAccelator() {
+        car.go();
+    }
+
+    public void stepBreak() {
+        car.stop();
+    }
+
+    public void turnOff() {
+        car.turnOff();
+    }
+}
